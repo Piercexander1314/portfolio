4876c75e18757e7508785e286c8aec7a20d167fe
Piercexander1314 <piercexander1314@gmail.com>
2026-01-03 01:00:33 +0800

    Update Projects.js

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -22,7 +22,6 @@ const projects = [
       "Social Media",
     ],
     images: [
-      "/images/projects/Social media posting.png",
       "/images/projects/Social media posting 1.png",
     ],
   },
